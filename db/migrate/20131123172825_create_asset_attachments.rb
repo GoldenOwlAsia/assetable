b43d8659df33f4165e09d6f3fa8e44dbc2d245ce
Nguyen Huu Phuoc <phuoc.nguyen@sstechvn.com>
2018-09-27 11:59:42 +0700

    Fix migration for rails 5

--- db/migrate/20131123172825_create_asset_attachments.rb
+++ db/migrate/20131123172825_create_asset_attachments.rb
@@ -7,9 +7,9 @@ class CreateAssetAttachments < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 
-    add_index :assetable_asset_attachments, :asset_id
-    add_index :assetable_asset_attachments, [:assetable_type, :assetable_id], name: "assetable_asset_attachments_poly_asset_attachment"
-    add_index :assetable_asset_attachments, [:assetable_type, :assetable_id, :name], unique: true, name: "assetable_asset_attachments_poly_named_asset"
-    add_index :assetable_asset_attachments, :assetable_id, name: "assetable_asset_attachments_assetable_id"
+    add_index(:assetable_asset_attachments, :asset_id)
+    add_index(:assetable_asset_attachments, [:assetable_type, :assetable_id], name: "assetable_asset_attachments_poly_asset_attachment")
+    add_index(:assetable_asset_attachments, [:assetable_type, :assetable_id, :name], unique: true, name: "assetable_asset_attachments_poly_named_asset")
+    add_index(:assetable_asset_attachments, :assetable_id, name: "assetable_asset_attachments_assetable_id")
   end
 end
